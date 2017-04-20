530a1490bbd96116f421a9a9972721be8d989e82
Dave Ingram <dave.ingram@acquia.com>
2017-04-20 14:09:16 -0400

    initial commit handles hmac and wraps a signed request

---
+++ index.js
@@ -0,0 +1,10 @@
+/*!
+ * acquia-lift
+ *
+ * Creates an interface to Acquia Lift
+ * See http://www.acquia.com/lift for more information
+ */
+
+'use strict';
+
+exports.signedRequest = require('./lib/signed-request');
